9c59b085eb9876b464dbb626b7962117c8f9a2fb
Jelle Versele <jelle.versele@internetarchitects.be>
2015-01-19 17:52:52 +0100

    Added specs.

---
+++ test/index.js
@@ -0,0 +1,53 @@
+describe('Overflow', function () {
+
+	'use strict';
+
+	var instance, $container;
+
+	beforeEach(function () {
+		$container = $('#')
+	});
+
+	afterEach(function () {
+
+	});
+
+	describe('setOverflow', function () {
+		it('should add the css class', function () {
+
+		});
+	});
+
+	describe('removeOverflow', function () {
+		it('should remove the css class')
+	});
+
+	describe('refresh', function () {
+		it('should add an overflowEnd css class', function () {
+
+		});
+
+		it('should add an overflowBegin css class', function () {
+
+		});
+	});
+
+	describe('init', function () {
+		it('should add css classes if needed', function () {
+
+		});
+	});
+
+	describe('getScrollableEl', function () {
+		it('should return the scrollable element', function () {
+
+		});
+	});
+
+	describe('destroy', function () {
+		it('should remove all overflow css classes', function () {
+
+		});
+	});
+
+});
