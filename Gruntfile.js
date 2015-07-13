274f096694f8195c1f66f9e932fb3e68bae6b469
Jelle Versele <jelle.versele@internetarchitects.be>
2015-07-13 15:55:45 +0200

    Remove outdated dependency from gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ var config = {
 		'bower_components/jquery/dist/jquery.js',
 		'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
 		'bower_components/pintxos-inherit/index.js',
-		'bower_components/pintxos-destroyable/index.js',
 		'bower_components/pintxos-component/index.js',
 		'bower_components/pintxos-scrollable_native/index.js',
 		'bower_components/pintxos-scrollable_ha/index.js',
